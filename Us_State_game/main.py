34c950bc0e5ccd45d1cfba0999ad00ee46ccdd71
Diogo-Rafael-Rodrigues <dumbexk@hotmail.com>
2021-06-10 15:17:24 -0300

    Us_state_game
    Educational game where the user tryies to fill all U.S states at the map

---
+++ Us_State_game/main.py
@@ -0,0 +1,36 @@
+import turtle
+import pandas
+
+screen = turtle.Screen()
+screen.title("U.S. State Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
+guessed_states = []
+missed_states = []
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
+                                    prompt="What's another state's name").title()
+
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.pu()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.color("Black")
+        t.write(answer_state)
+    elif answer_state == "Exit":
+        missed_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missed_states.append(state)
+        new_data = pandas.DataFrame(missed_states)
+        new_data.to_csv("states_to_learn.csv")
+        print(f" you missed {len(missed_states)}")
+        break
+
